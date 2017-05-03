c054ecb2e7443773f9d477348133fcddabad8601
egottcha <egottcha@users.noreply.github.com>
2017-05-03 09:15:32 +0200

    matplotlib canvas example added to webapp, req attention later

--- webapp/views.py
+++ webapp/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("<h1> Hey!! </h1>")
+    return render(request, 'webapp/home.html')
