bcc08f48c6621e7c6b466192aed5498c06c42128
egottcha <egottcha@users.noreply.github.com>
2017-06-26 08:56:00 +0200

    qlikmashup added

---
+++ QlikMashup/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class QlikmashupConfig(AppConfig):
+    name = 'QlikMashup'
--- djweb/settings.py
+++ djweb/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
     'blog',
     'tweet',
     'd3js',
+    'QlikMashup',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
